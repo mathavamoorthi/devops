eed6257f62488cf7f5ce9bfcf996936d43e91451
DPartha27 <partha05sarathy@gmail.com>
2026-05-07 11:25:00 +0530

    control-plane: add application_name and clearer pool error logging

--- control-plane/src/db.js
+++ control-plane/src/db.js
@@ -2,10 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  application_name: 'control-plane',
 });
 
 pool.on('error', (err) => {
-  console.error('Unexpected pg pool error:', err);
+  console.error('[db] pool error', err.code || '', err.message);
 });
 
 module.exports = pool;
